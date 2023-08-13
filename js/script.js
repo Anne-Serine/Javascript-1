8425e1b562904a3b3e6f3035764d2421561f20a7
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-08-14 00:09:27 +0200

    fetch product data from API, removed hardcodedhtml

--- js/script.js
+++ js/script.js
@@ -1,24 +1,44 @@
 const url = "https://api.noroff.dev/api/v1/rainy-days";
 
-const resultsContainer = document.querySelector(".results");
+const resultsContainer = document.querySelector(".product-card-container");
+
+function productCard(product) {
+  return `<a href="/products/scott-waterproof-jacket.html" class="product-card">
+  <div class="product-window">
+    <img src="${product.image}" class="jacket_image" alt="${product.title}">
+    <img src="/icons/heart-icon.svg" class="heart-icon" alt="heart-icon for favorites">
+  </div>
+  <p>${product.title}</p>
+  <div class="flex-container">
+    <p class="price">${product.price} NOK</p>
+    <div class="flex-container">
+      <div class="box light-grey"></div>
+      <div class="box pink"></div>
+      <div class="box sky-blue"></div>
+    </div>
+  </div>
+</a>`
+}
 
 
 async function getProducts() {
 
   const response = await fetch(url);
 
-  const results = await response.json();
+  const productData = await response.json();
 
-  const products = results;
 
-  //resultsContainer.innerHTML = "";
+  resultsContainer.innerHTML = "";
 
-  for (let i = 0; i < products.length; i++) {
-    console.log(products[i].title);
+  for (let i = 0; i < productData.length; i++) {
+    //console.log(productData[i].title);
 
-    //resultsContainer.innerHTML += `<div class="results">${products[i].title}<div>`;
+    
+
+    resultsContainer.innerHTML += productCard(productData[i]);
   }
 
 }
 
 getProducts();
+
