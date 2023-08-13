975a39aafb2da08f0bb6d0dc070e3915e44e5a3a
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-08-14 00:56:44 +0200

    fetch API on all category pages, removed hardcoded html on all category pages, added spinner-loading

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const url = "https://api.noroff.dev/api/v1/rainy-days";
 const resultsContainer = document.querySelector(".product-card-container");
 
 function productCard(product) {
-  return `<a href="/products/scott-waterproof-jacket.html" class="product-card">
+  return `<a href="/products/jacket.html?productId=${product.id}" class="product-card">
   <div class="product-window">
     <img src="${product.image}" class="jacket_image" alt="${product.title}">
     <img src="/icons/heart-icon.svg" class="heart-icon" alt="heart-icon for favorites">
@@ -32,8 +32,6 @@ async function getProducts() {
 
   for (let i = 0; i < productData.length; i++) {
     //console.log(productData[i].title);
-
-    
 
     resultsContainer.innerHTML += productCard(productData[i]);
   }
